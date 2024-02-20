38cc158e195dbd1f4178399c33432df2f8c82b64
nicholas-lok <nicholaslok@hotmail.co.uk>
2024-02-20 01:40:35 +0000

    Added VS Project Files and Structure

---
+++ Li2_GroundSegment/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <windows.h>
+#include <math.h>
+
+#include <TWSG.h>
+#include <Li2_CDI.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+
